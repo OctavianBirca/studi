3356ca92826cb3f56421d956e84eb4d5ac259d0d
OctavianBirca <121047796+OctavianBirca@users.noreply.github.com>
2024-08-12 21:00:19 +0200

    Add files via upload

---
+++ guess game/main.js
@@ -0,0 +1,14 @@
+const element = "elefant";
+
+const elementSplit = element.split("")
+
+
+function getLettre () {
+    let lettre = letter1.value;
+}
+   
+
+console.log(element);
+console.log(elementSplit);
+
+
